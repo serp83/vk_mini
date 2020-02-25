78f210a372db4024b24a0a10facf774d272d29ac
Tiurin Sergey <aser4giam@yandex.ru>
2020-02-26 00:47:04 +0300

    Updates

---
+++ precache-manifest.9887020676ec2332bb74bd5b7d06f87c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8f95e496cc67e586627e2aee164b4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a19073103c1f00233b69",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "394caed7e050c32cbc95",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a19073103c1f00233b69",
+    "url": "./static/js/2.6f05e32b.chunk.js"
+  },
+  {
+    "revision": "394caed7e050c32cbc95",
+    "url": "./static/js/main.295d8ad3.chunk.js"
+  },
+  {
+    "revision": "77ef4018a887752626e4",
+    "url": "./static/js/runtime-main.641239be.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
